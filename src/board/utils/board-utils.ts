4473e8ce1f12b582c8ca7d92aeef988aefc5d5d6
Stephen Gibson <sgibson21@qub.ac.uk>
2022-09-11 22:43:05 +0100

    setting groundwork for full FEN compatability

--- src/board/utils/board-utils.ts
+++ src/board/utils/board-utils.ts
@@ -308,10 +308,47 @@ export const initBoard: () => BoardState = () => {
     };
 }
 
+/**
+ * A FEN record contains six fields, each separated by a space. The fields are as follows:
+ *
+ * Piece placement data: Each rank is described, starting with rank 8 and ending with rank 1, with a "/" between each one;
+ * within each rank, the contents of the squares are described in order from the a-file to the h-file.
+ * Each piece is identified by a single letter taken from the standard English names in algebraic notation
+ * (pawn = "P", knight = "N", bishop = "B", rook = "R", queen = "Q" and king = "K").
+ * White pieces are designated using uppercase letters ("PNBRQK"), while black pieces use lowercase letters ("pnbrqk").
+ * A set of one or more consecutive empty squares within a rank is denoted by a digit from "1" to "8",
+ * corresponding to the number of squares.
+ *
+ * Active color: "w" means that White is to move; "b" means that Black is to move.
+ *
+ * Castling availability: If neither side has the ability to castle, this field uses the character "-".
+ * Otherwise, this field contains one or more letters: "K" if White can castle kingside, "Q" if White can castle queenside,
+ * "k" if Black can castle kingside, and "q" if Black can castle queenside. A situation that temporarily prevents castling
+ * does not prevent the use of this notation.
+ *
+ * En passant target square: This is a square over which a pawn has just passed while moving two squares;
+ * it is given in algebraic notation. If there is no en passant target square, this field uses the character "-".
+ * This is recorded regardless of whether there is a pawn in position to capture en passant.
+ * An updated version of the spec has since made it so the target square is only recorded if a legal en passant move
+ * is possible but the old version of the standard is the one most commonly used.
+ *
+ * Halfmove clock: The number of halfmoves since the last capture or pawn advance, used for the fifty-move rule.
+ *
+ * Fullmove number: The number of the full moves. It starts at 1 and is incremented after Black's move.
+ *
+ */
 export const loadPositionFromFen: (fen: string, state: BoardState) => BoardState = (fen: string, state: BoardState) => {
     state = {...state};
     state.squares = initSquares(); // clear state
 
+    const [piecesFen, activeColor, castling, enPassant, halfmoveClock, fullmoveNumber] = fen.split(' ');
+
+    state = loadFenPieces(piecesFen, state);
+
+    return state;
+}
+
+const loadFenPieces: (piecesFen: string, state: BoardState) => BoardState = (piecesFen: string, state: BoardState) => {
     const symbolMap = {
         k: KING,
         p: PAWN,
@@ -325,7 +362,7 @@ export const loadPositionFromFen: (fen: string, state: BoardState) => BoardState
 
     const unidentifiedPieces = [];
 
-    for (const symbol of fen) {
+    for (const symbol of piecesFen) {
         if (symbol === '/') {
             file = 'a';
             rank = getRankFrom(rank, -1);
@@ -347,7 +384,7 @@ export const loadPositionFromFen: (fen: string, state: BoardState) => BoardState
     unidentifiedPieces.forEach((up, index) => addPiece({...up.piece, id: index.toString()}, up.file, up.rank, state));
 
     return state;
-}
+};
 
 /**
  * Gets the coordinate { file, rank } of the square [fileCount] and [rankCount] away from the given square
